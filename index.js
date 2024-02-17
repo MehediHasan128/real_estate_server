c055e7df97e21d89cdaf39fe09b26dbcb25eae58
MehediHasan128 <mehedihasan12926@gmail.com>
2024-02-17 22:38:14 +0600

    create update api

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors());
 app.use(express.json());
@@ -72,7 +72,6 @@ async function run() {
     // Stored User Info
     app.post('/users', async(req, res) =>{
         const user = req.body;
-        console.log(user);
         const result = await userCollection.insertOne(user);
         res.send(result)
     })
@@ -101,7 +100,6 @@ async function run() {
     // Get user specific property
     app.get('/properties', async(req, res) =>{
       const email = req.query.email;
-      console.log(email);
       const query = {  ownerEmail: email }
       const result = await propertyCollection.find(query).toArray();
       res.send(result);
@@ -110,6 +108,24 @@ async function run() {
     app.get('/allProperty', async(req, res) =>{
       const result = await propertyCollection.find().toArray();
       res.send(result);
+    });
+
+
+    // Admin DashBoard
+    // Upadate property Status
+    app.put('/properties/:id', async(req, res) =>{
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const options = {upsert: true};
+      const updatePropertyStatus = req.body;
+      const updateStatus = {
+        $set: {  
+          status: updatePropertyStatus.propertyStatus
+        }
+      }
+      const result = await propertyCollection.updateOne(filter, updateStatus, options)
+      res.send(result)
+      console.log(updateStatus);
     })
 
 
